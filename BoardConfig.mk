e8380aad7e0c7dd507f112063ef9741db8704d07
Chris Fries <cfries@google.com>
2022-01-08 19:39:48 +0530

    X00T: Set VENDOR_SECURITY_PATCH to platform Set the VENDOR_SECURITY_PATCH level to match the platform security patch level.  These are functionally the same but since our vendor is old it shows as old device in some apps and says potential outdated stuff warning etc so better spoof it to the current build date to show as update one (THis is kinda also needed as we update our vendor from other device/caf blobs but then this date isnt updated so lets use this method)
    Change-Id: Ib77ab115d6faabd21958c34e2dae49ec96421fdd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-08-05
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
